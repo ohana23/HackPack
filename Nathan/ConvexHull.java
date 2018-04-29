15ca1b47a5fecbcb40ca76878a05dfe06736e370
Nathan Tackett <nathanptackett@outlook.com>
2018-04-29 11:48:58 -0400

    Added ConvexHull and PointInPolygon code. ConvexHull needs to be compressed and edited, PointInPolygon is not working yet because of the LineIntersect Function (I don't think it works how I think it works).

--- Nathan/ConvexHull.java
+++ Nathan/ConvexHull.java
@@ -1,8 +1,129 @@
+/*
+    Code pulled from Arup Guha's cop4516 class.
+*/
+
 import java.io.*;
+import java.util.*;
 
 public class ConvexHull {
     public static void main(String args[])
     {
-        System.out.println("Hello world!");
+        //before you start using the pt functions. You need to set
+        //the ref point. This is the point that all vectors will be based on.
+        //You can use getIndexMin to get the left most, down most point.
+        //and set that x and y to ref x
     }
+
+    // Returns the point in pts with minimum y breaking tie by minimum x.
+	public static int getIndexMin(pt[] pts, int n) {
+		int res = 0;
+		for (int i=1; i<n; i++)
+			if (pts[i].y < pts[res].y || (pts[i].y == pts[res].y && pts[i].x < pts[res].x))
+				res = i;
+		return res;
+	}
+
+    public static double grahamScan(pt[] pts, int n) {
+
+        // Sort the points by angle with reference point.
+        Arrays.sort(pts);
+
+        // Push first two points on.
+        Stack<pt> myStack = new Stack<pt>();
+        myStack.push(pts[0]);
+        myStack.push(pts[1]);
+
+        // Go through the rest of the points.
+        for (int i=2; i<n; i++) {
+
+            // Get last three pts.
+            pt cur = pts[i];
+            pt mid = myStack.pop();
+            pt prev = myStack.pop();
+
+            // Pop off the left turns.
+            while (!prev.isRightTurn(mid, cur)) {
+                mid = prev;
+                prev = myStack.pop();
+            }
+
+            // Push back the last right turn.
+            myStack.push(prev);
+            myStack.push(mid);
+            myStack.push(cur);
+        }
+
+        // Add up distances around the hull.
+        double res = 0;
+        pt cur = pts[0];
+        while (myStack.size() > 0) {
+            pt next = myStack.pop();
+            res += cur.dist(next);
+            cur = next;
+        }
+
+        // Return.
+        return res;
+    }
+}
+
+class pt implements Comparable<pt> {
+
+	// Stores reference pt
+	public static int refX;
+	public static int refY;
+
+	public int x;
+	public int y;
+
+	public pt(int myx, int myy) {
+		x = myx;
+		y = myy;
+	}
+
+	// Returns the vector from this to other.
+	public pt getVect(pt other) {
+		return new pt(other.x-x, other.y-y);
+	}
+
+	// Returns the distance between this and other.
+	public double dist(pt other) {
+		return Math.sqrt((other.x-x)*(other.x-x) + (other.y-y)*(other.y-y));
+	}
+
+	// Returns the magnitude ot this cross product other.
+	public int crossProductMag(pt other) {
+		return this.x*other.y - other.x*this.y;
+	}
+
+	// returns true iff this to mid to next is a right turn (180 degree is considered right turn).
+	public boolean isRightTurn(pt mid, pt next) {
+		pt v1 = getVect(mid);
+		pt v2 = mid.getVect(next);
+		return v1.crossProductMag(v2) >= 0; /*** Change to > 0 to skip collinear points. ***/
+	}
+
+	// Returns true iff this pt is the origin.
+	public boolean isZero() {
+		return x == 0 && y == 0;
+	}
+
+	public int compareTo(pt other) {
+
+		pt myRef = new pt(refX, refY);
+		pt v1 = myRef.getVect(this);
+		pt v2 = myRef.getVect(other);
+
+		// To avoid 0 issues.
+		if (v1.isZero()) return -1;
+		if (v2.isZero()) return 1;
+
+		// Angles are different, we are going counter-clockwise here.
+		if (v1.crossProductMag(v2) != 0)
+			return -v1.crossProductMag(v2);
+
+		// This should work, smaller vectors come first.
+		if (myRef.dist(v1) < myRef.dist(v2)) return -1;
+		return 1;
+	}
 }
--- Nathan/PointInPolygon.java
+++ Nathan/PointInPolygon.java
@@ -1,8 +1,61 @@
 import java.io.*;
 
 public class PointInPolygon {
+    public final static int oo = 1000000;
+    public final static double EPSILON = 1e-9;
+
+    public static boolean PIP(Point p, Point[] pg) {
+        int l = pg.length;
+        if(l < 3) return false; //three points are required for a Polygon
+
+        //create a point for the infinite line segment
+        Point infP = new Point(oo, p.y);
+        Point intersection;
+
+        //count the number of intersection of the above line with sides of polygon
+        int numInter = 0;
+        for(int i = 0; i < l; i++) {
+            int next = (i + 1) % l;
+            //see if p -> infP intersects polygon[i] -> polygon[next]
+            if((intersection = LLIntersect(p.x, p.y, infP.x, infP.y, pg[i].x, pg[i].y, pg[next].x, pg[next].y)) != null) {
+                System.out.println("Found intersection at "+intersection);
+                numInter++;
+            }
+        }
+
+        System.out.println("There were "+numInter+" intersections");
+        return (numInter % 2) == 1;
+    }
+
+    //Daniels Code
+    public static Point LLIntersect(double x1, double y1, double x2, double y2,
+        double x3, double y3, double x4, double y4) {
+        double d = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4);
+        if (Math.abs(d) < EPSILON) return null;
+        double xi = ((x3-x4)*(x1*y2-y1*x2)-(x1-x2)*(x3*y4-y3*x4))/d;
+        double yi = ((y3-y4)*(x1*y2-y1*x2)-(y1-y2)*(x3*y4-y3*x4))/d;
+        return (new Point(xi, yi));
+    }
+
     public static void main(String args[])
     {
-        System.out.println("Hello world!");
+        Point[] pg = {new Point(0, 0), new Point(10, 0), new Point(10, 10), new Point(0, 10)};
+        Point insideP = new Point(5, 5);
+        Point outsideP = new Point(20, 20);
+        System.out.println("This should be true: "+PIP(insideP, pg));
     }
+}
+
+class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public String toString() {
+      return String.format("(%f, %f)", x, y);
+  }
 }
