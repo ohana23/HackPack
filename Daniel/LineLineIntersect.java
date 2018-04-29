f4bc93dc7d14d1bb04e85a60d1bb443a267af337
Daniel Ohana <ohana23@users.noreply.github.com>
2018-04-29 12:16:56 -0400

    Update LineLineIntersect.java

--- Daniel/LineLineIntersect.java
+++ Daniel/LineLineIntersect.java
@@ -4,16 +4,25 @@
 public class LineLineIntersect {
   public static void main(String[] args) {
     Point P = LLIntersect(1,1,5,6,3,5,6,2);
-    System.out.printf("%.2f, %.2f\n", P.x, P.y);
+
+    if (P != null)
+      System.out.printf("%.2f, %.2f\n", P.x, P.y);
   }
 
+  // This returns a point if the two lines intersect or null if they don't.
+  // (x1, y1) and (x2, y2) are the starting and ending points for line 1.
+  // (x3, y3) and (x4, y4) are the starting and ending points for line 2.
   public static Point LLIntersect(double x1, double y1, double x2, double y2, 
     double x3, double y3, double x4, double y4) {
     
     double d = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4);
-    if (d == 0) return null;
-    double xi = ((x3-x4)*(x1*y2-y1*x2)-(x1-x2)*(x3*y4-y3*x4))/d;
-    double yi = ((y3-y4)*(x1*y2-y1*x2)-(y1-y2)*(x3*y4-y3*x4))/d;
+
+    if (d == 0)
+      return null;
+    
+    double xi = ((x3-x4)*(x1*y2-y1*x2) - (x1-x2)*(x3*y4-y3*x4)) / d;
+    double yi = ((y3-y4)*(x1*y2-y1*x2) - (y1-y2)*(x3*y4-y3*x4)) / d;
+
     return (new Point(xi, yi));
   }
 }
